478f4cd5c6bdd94f9b2267aba15a0015b78fc95a
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-06-15 16:01:50 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { connection } = require("./mongoose/connection");
-const { userRoutes } = require("./routes/userRoutes");
 const { blogRoutes } = require("./routes/blogRoutes");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const { authentication } = require("./middleware/authentication");
 const cors = require("cors");
+const { userModel } = require("./model/userModel");
 
 const app = express();
 app.use(express.json());
@@ -13,7 +15,49 @@ app.get("/", (req, res) => {
     res.send("Server is up and running");
 })
 
-app.use("/api", userRoutes);
+app.post("/api/register", async (req, res) => {
+    const { username, avatar, email, password } = req.body;
+    if (!username, !avatar, !email, !password) {
+        res.status(400).send({ msg: "invalid data format" });
+    } else {
+        try {
+            const emailCheck = await userModel.findOne({ email });
+            const userCheck = await userModel.findOne({ username });
+            if (emailCheck || userCheck) {
+                res.status(400).send({ msg: "user already present" });
+            } else {
+                const newUser = new userModel({ username, avatar, email, password: bcrypt.hashSync(password, 4) });
+                await newUser.save();
+                res.status(200).send({ msg: "new user created", data: { username, avatar, email } });
+            }
+        } catch (e) {
+            res.send({ msg: e.message });
+        }
+    }
+})
+
+app.post("/api/login", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email, !password) {
+        res.status(400).send({ msg: "invalid data format" });
+    } else {
+        try {
+            const emailCheck = await userModel.findOne({ email });
+            if (!emailCheck) {
+                res.status(400).send({ msg: "user not found" });
+            } else {
+                const verify = bcrypt.compareSync(password, emailCheck.password);
+                if (!verify) {
+                    res.status(400).send({ msg: "incorrect password" });
+                } else {
+                    res.status(200).send({ msg: "user logged in", token: jwt.sign({ uid: emailCheck._id }, "blogapp") });
+                }
+            }
+        } catch (e) {
+            res.send({ msg: e.message });
+        }
+    }
+})
 
 app.use(authentication);
 
